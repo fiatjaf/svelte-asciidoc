7ff9b9517397b0cd706b7de09189cacc6cb5a169
fiatjaf <fiatjaf@gmail.com>
2024-06-15 08:40:01 -0300

    use custom renderers.

--- src/lib/Block.svelte
+++ src/lib/Block.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+  import {getCustomRenderers} from './'
   import {type Renderers, defaultRenderers} from './renderers'
   import Unsupported from './renderers/Unsupported.svelte'
 
   export let node: any
 
   const type = node.getNodeName() as keyof Renderers
-  const component = defaultRenderers[type] || Unsupported
+
+  const overrides = getCustomRenderers()
+  const component = overrides[type] || defaultRenderers[type] || Unsupported
 </script>
 
 <svelte:component this={component} {node} />
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
-import {setContext} from 'svelte'
+import {getContext, setContext} from 'svelte'
 import SvelteAsciidoc from './SvelteAsciidoc.svelte'
 import type {Renderers} from './renderers'
 
@@ -8,8 +8,14 @@ const extraKey = Symbol('extrakey')
 export function setExtra(extra: any) {
   setContext(extraKey, extra)
 }
+export function getExtra(): any {
+  return getContext(extraKey)
+}
 
 const customRenderersKey = Symbol('customRenderers')
 export function setCustomRenderers(cr: Partial<Renderers>) {
   setContext(customRenderersKey, cr)
+}
+export function getCustomRenderers(): Partial<Renderers> {
+  return getContext(customRenderersKey) || {}
 }
