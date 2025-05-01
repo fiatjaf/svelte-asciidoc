bc7b642ed4f84509cfd22f3a9d7cd2a699d7e33e
fiatjaf <fiatjaf@gmail.com>
2025-05-01 08:55:43 -0300

    fix some $effect garbage.

--- src/lib/SvelteAsciidoc.svelte
+++ src/lib/SvelteAsciidoc.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-  import { run } from 'svelte/legacy';
-
-  import {createEventDispatcher} from 'svelte'
   import asciidoctor, {type Document} from '@asciidoctor/core'
 
   import Block from './Block.svelte'
@@ -11,11 +8,12 @@
   import {setCustomRenderers, setNaturalRenderers, setExtra} from './'
 
   interface Props {
-    source: string;
-    extra?: any;
-    customRenderers?: Partial<Renderers>;
-    naturalRenderers?: NaturalRenderers;
-    supportMarkdownTransition?: boolean;
+    source: string
+    extra?: any
+    customRenderers?: Partial<Renderers>
+    naturalRenderers?: NaturalRenderers
+    supportMarkdownTransition?: boolean
+    onParsed?: (doc: Document) => void
   }
 
   let {
@@ -23,102 +21,98 @@
     extra = null,
     customRenderers = {},
     naturalRenderers = {},
-    supportMarkdownTransition = false
-  }: Props = $props();
+    supportMarkdownTransition = false,
+    onParsed
+  }: Props = $props()
 
   setCustomRenderers(customRenderers)
   setNaturalRenderers(naturalRenderers)
   setExtra(extra)
 
-  const dispatch = createEventDispatcher()
+  let doc = $state<Document>()
+  let authors = $state<Document.Author[]>([])
+  let footnotes = $state<Document.Footnote[]>([])
 
-  let doc: Document = $state()
-
-  run(() => {
+  $effect(() => {
     const Asciidoctor = asciidoctor()
     ;(Asciidoctor as any).Compliance.markdown_syntax = supportMarkdownTransition
-    doc = Asciidoctor.load(source)
-    dispatch('parsed', doc)
-  });
+    let parsed = Asciidoctor.load(source)
+    onParsed?.(parsed)
 
-  let blocks = $derived(doc.getBlocks())
-  let footnotes = $derived(doc.getFootnotes())
-  let nofootnotes =
-    $derived(blocks.length > 0 && blocks[0].getDocument().hasAttribute('nofootnotes'))
-  let noheader = $derived(doc.getNoheader())
-  let hasheader = $derived(doc.hasHeader())
-  let toc =
-    $derived(doc.hasSections() &&
-    doc.hasAttribute('toc') &&
-    doc.getAttribute('toc-placement') === 'auto')
-  let authors = $derived(doc.getAuthors())
-  let detail =
-    $derived(authors.length > 0 ||
-    doc.hasAttribute('revnumber') ||
-    doc.hasAttribute('revdate') ||
-    doc.hasAttribute('revremark'))
+    footnotes = parsed.getFootnotes()
+    authors = parsed.getAuthors()
+    doc = parsed
+  })
 </script>
 
-{#if !noheader}
-  <header id="header">
-    {#if hasheader}
-      <h1><Html raw={doc.getDocumentTitle()?.toString()} /></h1>
-    {/if}
-    {#if detail}
-      <div class="details">
-        {#each authors as author, index}
-          {#if author.getName()}
-            <span id={`author${index + 1 > 1 ? index + 1 : ''}`} class="author">
-              {doc.applySubstitutions(author.getName() || '').toString()}
-            </span>
-            <br />
+{#if doc}
+  {#if !doc.getNoheader()}
+    <header id="header">
+      {#if doc.hasHeader()}
+        <h1><Html raw={doc.getDocumentTitle()?.toString()} /></h1>
+      {/if}
+      {#if authors.length > 0 || doc.hasAttribute('revnumber') || doc.hasAttribute('revdate') || doc.hasAttribute('revremark')}
+        <div class="details">
+          {#each authors as author, index}
+            {#if author.getName()}
+              <span
+                id={`author${index + 1 > 1 ? index + 1 : ''}`}
+                class="author"
+              >
+                {doc.applySubstitutions(author.getName() || '').toString()}
+              </span>
+              <br />
+            {/if}
+            {#if author.getEmail()}
+              <span id={`email${index + 1 > 1 ? index + 1 : ''}`} class="email">
+                {doc.applySubstitutions(author.getEmail() || '').toString()}
+              </span>
+              <br />
+            {/if}
+          {/each}
+          {#if doc.hasAttribute('revnumber')}
+            <span id="revnumber"
+              >{`${doc
+                .getAttribute('version-label')
+                .toLowerCase()} ${doc.getAttribute('revnumber')}${
+                doc.hasAttribute('revdate') ? ',' : ''
+              }`}</span
+            >
           {/if}
-          {#if author.getEmail()}
-            <span id={`email${index + 1 > 1 ? index + 1 : ''}`} class="email">
-              {doc.applySubstitutions(author.getEmail() || '').toString()}
-            </span>
-            <br />
+          {#if doc.hasAttribute('revdate')}
+            <span id="revdate">{doc.getAttribute('revdate')}</span>
           {/if}
-        {/each}
-        {#if doc.hasAttribute('revnumber')}
-          <span id="revnumber"
-            >{`${doc
-              .getAttribute('version-label')
-              .toLowerCase()} ${doc.getAttribute('revnumber')}${
-              doc.hasAttribute('revdate') ? ',' : ''
-            }`}</span
-          >
-        {/if}
-        {#if doc.hasAttribute('revdate')}
-          <span id="revdate">{doc.getAttribute('revdate')}</span>
-        {/if}
-        {#if doc.hasAttribute('revremark')}
-          <br />
-          <span id="revremark">{doc.getAttribute('revremark')}</span>
-        {/if}
-      </div>
-    {/if}
-    {#if toc}
-      <div id="toc" class={doc.getAttribute('toc-class', 'toc')}>
-        <div id="toctitle">{doc.getAttribute('toc-title')}</div>
-        <Outline node={doc} sectNumLevels={0} tocLevels={2} />
-      </div>
-    {/if}
-  </header>
-{/if}
-{#each blocks as block}
-  <Block node={block} />
-{/each}
-{#if footnotes.length > 0 && !nofootnotes}
-  <footer id="footnotes">
-    <hr />
-    {#each footnotes as footnote (footnote.getIndex())}
-      <div class="footnote" id={`_footnotedef_${footnote.getIndex()}`}>
-        <a href={`#_footnoteref_${footnote.getIndex()}`}
-          >{footnote.getIndex()}</a
-        >.
-        {footnote.getText()}
-      </div>
-    {/each}
-  </footer>
+          {#if doc.hasAttribute('revremark')}
+            <br />
+            <span id="revremark">{doc.getAttribute('revremark')}</span>
+          {/if}
+        </div>
+      {/if}
+      {#if doc.hasSections() && doc.hasAttribute('toc') && doc.getAttribute('toc-placement') === 'auto'}
+        <div id="toc" class={doc.getAttribute('toc-class', 'toc')}>
+          <div id="toctitle">{doc.getAttribute('toc-title')}</div>
+          <Outline node={doc} sectNumLevelsAttr="0" tocLevelsAttr="2" />
+        </div>
+      {/if}
+    </header>
+  {/if}
+  {#each doc.getBlocks() as block}
+    <Block node={block} />
+  {/each}
+  {#if footnotes.length > 0 && !(doc.getBlocks().length > 0 && doc
+        .getBlocks()[0]
+        .getDocument()
+        .hasAttribute('nofootnotes'))}
+    <footer id="footnotes">
+      <hr />
+      {#each footnotes as footnote (footnote.getIndex())}
+        <div class="footnote" id={`_footnotedef_${footnote.getIndex()}`}>
+          <a href={`#_footnoteref_${footnote.getIndex()}`}
+            >{footnote.getIndex()}</a
+          >.
+          {footnote.getText()}
+        </div>
+      {/each}
+    </footer>
+  {/if}
 {/if}
